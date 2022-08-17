556918cdc57f82d130baafb50c65825bb2447be6
Abdalla Jama <abdallajama201@gmail.com>
2022-08-17 17:59:05 -0400

    nav and header complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import './App.css';
-import Button from '@mui/material/Button';
+import React, { useState } from 'react'
+import Home from './components/pages/Home';
+import Projects from './components/pages/Projects';
+import Resume from './components/pages/Resume';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 function App() {
+  // Conditionally render page based on handelPageChange function
+  const [currentPage, setCurrentPage] = useState('Home');
+  const renderPage = () => {
+    if(currentPage === 'Projects') {
+      return <Projects />;
+    }
+    else if(currentPage === 'Resume') {
+      return <Resume />;
+    } else {
+      return <Home />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
-    <Button variant="contained">Hello World</Button>
+    <div>
+      <Header currentPage={currentPage} handlePageChange={handlePageChange}/>
+      {/* {renderPage()} */}
+      {/* <Footer /> */}
+    </div>
   );
 }
 
