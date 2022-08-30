9e408a91fe29bbbe4bd7f57c6d4996c8a72ce1dc
Abdalla Jama <abdallajama201@gmail.com>
2022-08-30 10:23:54 -0400

    logo change and styling fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,18 +7,21 @@ import '@mui/system';
 import '../styles/Header.scss'
 
 const pages = ['Home', 'Projects', 'Resume'];
+// const portrait = 'https://cdn.jsdelivr.net/gh/abdallajama201/portfolio-v3@main/src/img/portrait.png'
 
 export default function Header({currentPage, handlePageChange}) {
   return (
-    <Grid id='grid-header' container item xs={30} direction="row" justifyContent="space-between" alignItems="center">
-      <Tooltip title='rumble rumble'>
-        <img src={portrait} id='portrait' alt='portrait'></img>
-      </Tooltip>
-      <Grid container item xs={2} sm={4} md={8} direction="row" justifyContent="space-around" alignItems="center">
-        {pages.map((page, index) => 
-          <Typography id='Typography' variant='h4' key={index} href={"#"+page} className={`custom-btn btn ${currentPage === page ? 'active' : 'inactive'}`} onClick={() => handlePageChange(page)}><span>{page}</span><span>{page}</span></Typography>)}
+    <Grid id='grid-header' container item xs={20} direction="row" justifyContent="space-between" alignItems="center">
+      <Grid id='grid-header' container item xs={2} sm={4} md={20} direction="row" justifyContent="space-between" alignItems="center">
+        <Tooltip title='rumble rumble'>
+          <img src={portrait} id='portrait' alt='portrait'></img>
+        </Tooltip>
+        <Grid container item xs={2} sm={4} md={8} direction="row" justifyContent="space-around" alignItems="center">
+          {pages.map((page, index) => 
+            <Typography id='Typography' variant='h4' key={index} href={"#"+page} className={`custom-btn btn ${currentPage === page ? 'active' : 'inactive'}`} onClick={() => handlePageChange(page)}><span>{page}</span><span>{page}</span></Typography>)}
+        </Grid>
+        <TokenOutlinedIcon id='spinner' />
       </Grid>
-      <TokenOutlinedIcon id='spinner' />
     </Grid>
   );
 } 
